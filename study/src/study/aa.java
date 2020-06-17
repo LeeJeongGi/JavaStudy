77708b7366504bf3f3ecdf4d63f2f684974a5af7
LeeJeongGi <apple4rhk@naver.com>
2020-06-17 12:37:24 +0900

    자바 기본적인 공부 시작

---
+++ study/src/study/aa.java
@@ -0,0 +1,19 @@
+package study;
+
+public class aa {
+	public static void main(String[] args) {
+		
+		int x = 3;
+		int y = 2;
+		int sum = Sum(x, y);
+		System.out.println("гую╨ : " + sum);
+		
+		System.out.println("x + y : " + (x + y));
+	}
+	
+	public static int Sum(int x, int y) {
+		
+		x = 2;
+		return x+y;
+	}
+}
